434801846c541f35f653927d44943d4b71f77461
Hafiz Rizwan <hafizmr7788@gmail.com>
2026-07-08 09:36:28 +0500

    Class 22 add the sms service

---
+++ Class_22/services/seller.py
@@ -0,0 +1,19 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from api.schemas.seller import SellerCreate
+from database.models import Seller
+from services.user import UserService
+
+
+class SellerService(UserService):
+    def __init__(self,session: AsyncSession,tasks):
+        super().__init__(Seller,session,tasks)
+        
+    async def add(self,seller_create: SellerCreate)->Seller:
+        return await self._add_user(
+            seller_create.model_dump(),
+            "seller"
+        )
+    
+    
+    async def token(self,email,password)->str:
+        return await self._generate_token(email,password)
